00bdd0988508e45e0e65ca7149074177e4d2a60a
1155476 <hanyd1012@hyundai-autoever.com>
2025-02-10 10:52:54 +0900

    Singleton in spring by @configuration

---
+++ src/test/java/com/bigdata/bricks/singleton/SingletonTest.java
@@ -0,0 +1,76 @@
+package com.bigdata.bricks.singleton;
+
+import com.bigdata.bricks.AppConfig;
+import com.bigdata.bricks.member.MemberRepository;
+import com.bigdata.bricks.member.MemberService;
+import com.bigdata.bricks.member.MemberServiceImpl;
+import com.bigdata.bricks.order.OrderService;
+import com.bigdata.bricks.order.OrderServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("스프링 없는 순사한 DI 컨테이너")
+    void pureContainer() {
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService1 = appConfig.memberService();
+        MemberService memberService2 = appConfig.memberService();
+
+        System.out.println(memberService1);
+        System.out.println(memberService2);
+        Assertions.assertNotEquals(memberService1, memberService2);
+    }
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    public void singletonServiceTest() {
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println(singletonService1);
+        System.out.println(singletonService2);
+
+        Assertions.assertEquals(singletonService1, singletonService2);
+        singletonService1.logic();
+        singletonService2.logic();
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        //1. 조회: 호출할 때 마다 같은 객체를 반환
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        //2. 조회: 호출할 때 마다 같은 객체를 반환
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+        //참조값이 같은 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+        Assertions.assertEquals(memberService1, memberService2);
+
+    }
+
+    @Test
+    @DisplayName("Configuration 내에서 중복된 new 처리 확인")
+    void configurationTest() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+        //모두 같은 인스턴스를 참고하고 있다.
+        System.out.println("memberService -> memberRepository = " + memberService.getMemberRepository());
+        System.out.println("orderService -> memberRepository = " + orderService.getMemberRepository());
+        System.out.println("memberRepository = " + memberRepository);//모두 같은 인스턴스를 참고하고 있다.
+        Assertions.assertEquals(memberService.getMemberRepository(), memberRepository);
+        Assertions.assertEquals(orderService.getMemberRepository(), memberRepository);
+
+
+    }
+
+
+}
